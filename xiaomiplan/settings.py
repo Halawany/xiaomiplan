0d3d03f6167db307f98628154b2649abec58cb02
Eslam El-Halawany <halawany.me@gmail.com>
2024-04-23 13:31:31 +0200

    Added postgresql credentials

--- xiaomiplan/settings.py
+++ xiaomiplan/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'xiaomiplan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'eslam010',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
