1ee251ae1e9f86f7b79edbacf52d8da8ec97d670
Eslam Halawany <halawany.me@gmail.com>
2024-05-14 20:11:52 +0300

    Added Update plan feature

--- miplan/forms.py
+++ miplan/forms.py
@@ -6,3 +6,10 @@ class InsertForm(forms.Form):
     first_name = forms.CharField(max_length=50, required=True, help_text="Enter First Name")
     last_name = forms.CharField(max_length=50, required=True, help_text="Enter Last Name")
     
+
+class UpdateForm(forms.Form):
+    
+    id = forms.IntegerField(help_text="Enter Row ID number", required=True)
+    first_name = forms.CharField(max_length=50, required=True, help_text="Enter First Name")
+    last_name = forms.CharField(max_length=50, required=True, help_text="Enter Last Name")
+    
--- miplan/views.py
+++ miplan/views.py
@@ -3,13 +3,13 @@ from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 import psycopg2
 
-from .forms import InsertForm
+from .forms import InsertForm, UpdateForm
 
 def plan_home_view(request):
     try:
         conn = psycopg2.connect("dbname=plan_test_db user=postgres password=eslam010")
         with conn.cursor() as cur:
-            cur.execute("SELECT * FROM users")
+            cur.execute("SELECT * FROM users ORDER BY id;")
             data = cur.fetchall()
             context = {'all_data': data}
             return render(request, 'miplan/home.html', context)
@@ -31,7 +31,7 @@ def insert_plan_view(request):
                 conn = psycopg2.connect("dbname=plan_test_db user=postgres password=eslam010")
                 with conn.cursor() as cur:
                     cur.execute("SELECT id FROM users ORDER BY id DESC;")
-                    last_id= cur.fetchone()[0] + 1
+                    last_id = cur.fetchone()[0] + 1
                     cur.execute("INSERT INTO users (id, first_name, last_name) VALUES (%s, %s, %s);", (last_id, first_name, last_name))
                     conn.commit()
                     conn.close()
@@ -47,7 +47,7 @@ def insert_plan_view(request):
 
 def update_plan_view(request):
     if request.method == "POST":
-        form = InsertForm(request.POST)
+        form = UpdateForm(request.POST)
         if form.is_valid():
             id = form.cleaned_data["id"]
             first_name = form.cleaned_data["first_name"]
@@ -55,11 +55,14 @@ def update_plan_view(request):
             try:
                 conn = psycopg2.connect("dbname=plan_test_db user=postgres password=eslam010")
                 with conn.cursor() as cur:
-                    cur.execute("UPDATE users SET first_name = %s, last_name = %s WHERE id = %s;", (first_name, last_name, id))
+                    cur.execute("UPDATE users SET first_name = %s, last_name = %s WHERE id = %s", (first_name, last_name, id))
                     conn.commit()
                     conn.close()
                     return redirect('home')
             except psycopg2.Error as e:
                 print("Error occured while inserting data:", e)
                 context = {"error_message": e}
-                return render(request, 'miplan/insert.html', context)
+                return render(request, 'miplan/update.html', context)
+    else:
+        form = InsertForm()
+    return render(request, "miplan/update.html", {"form": form})
